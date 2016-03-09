eba031ddddaafff8f58c1ec6d64a399897118c51
Fuzz Worley <bitcopia@gmail.com>
2016-03-09 15:28:47 -0800

    add test line to layout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  # config.assets.compile = true
+  # config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
