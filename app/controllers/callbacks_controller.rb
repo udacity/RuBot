86518ed956ad49f725bf2b08d32d9638bbbb23be
Fuzz Worley <bitcopia@gmail.com>
2016-03-21 16:56:48 -0700

    handle auth error better and add flash message for error

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,11 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-    def google_oauth2
-        @admin = Admin.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @admin
+  def google_oauth2
+    @admin = Admin.from_omniauth(request.env["omniauth.auth"])
+    if @admin.email != ""
+      sign_in_and_redirect @admin
+    else
+      puts "Auth ERROR!!!"
+      redirect_to new_admin_session_path, flash: {error: 'You must log in with your Udacity or Knowlabs email address.' } 
     end
+  end
 end
