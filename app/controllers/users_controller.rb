7dfccce1eb37ee7c1dfcc06266eaf50a77af1a04
Fuzz Worley <bitcopia@gmail.com>
2016-03-23 15:46:55 -0700

    sort users by name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order(:user_name)
   end
 
   # GET /users/1
