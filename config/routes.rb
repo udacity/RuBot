df8c386310ee89b3bdd46bd9fb987d3f7c65544e
Fuzz Worley <bitcopia@gmail.com>
2016-03-23 23:06:35 -0700

    add metrics for interactions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :messages
   resources :users
+  resources :metrics
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
