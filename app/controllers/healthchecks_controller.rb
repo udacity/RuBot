02f26258161a15ce289d701d6102ca318803c836
Fuzz Worley <bitcopia@gmail.com>
2016-04-21 12:24:19 -0700

    logging

--- app/controllers/healthchecks_controller.rb
+++ app/controllers/healthchecks_controller.rb
@@ -2,7 +2,9 @@ class HealthchecksController < ApplicationController
   skip_before_action :authenticate_admin!
 
   def check
+    puts "Start Healthcheck"
     status = Healthcheck.get_presence(Rails.application.config.client) ? 200 : 404
+    puts "Status: #{status}"
     render :nothing => true, :status => status
   end
 
--- lib/healthcheck.rb
+++ lib/healthcheck.rb
@@ -3,6 +3,7 @@ module Healthcheck
   def self.get_presence(client)
     presence = client.web_client.users_getPresence(user: "U0RDVBPCH")
     presence.online
+    puts "Presence: #{presence}"
   end
 
 end
