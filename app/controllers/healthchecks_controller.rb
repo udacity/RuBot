462262d9c4eec0cdb26b26f96d14d2e9e6c0d971
Fuzz Worley <bitcopia@gmail.com>
2016-04-21 11:26:05 -0700

    refactor healthcheck

--- app/controllers/healthchecks_controller.rb
+++ app/controllers/healthchecks_controller.rb
@@ -2,11 +2,8 @@ class HealthchecksController < ApplicationController
   skip_before_action :authenticate_admin!
 
   def check
-    if Healthcheck.get_presence(Rails.application.config.client) == true
-      render :nothing => true, :status => 200
-    else
-      render :nothing => true, :status => 404
-    end
+    status = Healthcheck.get_presence(Rails.application.config.client) ? 200 : 404
+    render :nothing => true, :status => status
   end
 
 end
