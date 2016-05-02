cd6d5cc33977c31071fd1fe1b1345eb6ff3ae668
Jarrod Parkes <jarrod@udacity.com>
2016-05-02 16:39:16 -0500

    change app constants for swift

--- config/application.rb
+++ config/application.rb
@@ -28,15 +28,15 @@ module Rubot
     config.after_initialize do
       puts "ENV = development? #{Rails.env.development?}"
       
-      Rails.application.config.client_name = "Rubot"
-      Rails.application.config.ndkey = "nd010"
+      Rails.application.config.client_name = "Swiftbot"
+      Rails.application.config.ndkey = "nd003"
       Rails.application.config.standard_responses = 
         [
           "I'm sorry, I didn't understand that. For a list of commands, type `help`.",
           "I'm just a little bot who doesn't know a lot, but type `help` to see what I can respond to!",
           "Hmmmm, I haven't learned that one yet. Type `help` to see what I do know!",
           "That sounds interesting, but my bot brain has no idea what it means! Type `help` to see what I can respond to.",
-          "Ahhh, to be fluent in English! I only know Ruby. Type `help` to see the limited English that I understand."
+          "Ahhh, to be fluent in English! I only know Swift. Type `help` to see the limited English that I understand."
         ]
 
       Thread.new do
