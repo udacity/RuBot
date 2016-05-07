e0bcfa215bd611b98c038977922aea18850a7c91
Fuzz Worley <bitcopia@gmail.com>
2016-05-07 14:20:36 -0700

    add rspec

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,6 @@
-require 'test_helper'
+require 'minitest'
 
-class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class ClientTest < MiniTest::Test
+
+
 end
